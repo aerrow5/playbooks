0aa5ff9de1c3648f5235e2463e80a7894aa81ab0
Andrew Klaus <andrewklaus@gmail.com>
2017-01-13 23:50:11 -0700

    Fixing plex version script

--- roles/plex/files/get_latest_plex_version.py
+++ roles/plex/files/get_latest_plex_version.py
@@ -1,26 +1,25 @@
-from bs4 import BeautifulSoup
 import requests
 import sys
 import re
 
 if (sys.version_info > (3, 0)):
-    # Python 3 code in this block
     from urllib import parse
     py_ver = 3
 else:
-    # Python 2 code in this block
     from urlparse import urlparse
     py_ver = 2
 
+r = requests.get("https://plex.tv/api/downloads/1.json")
 
-page = requests.get("https://plex.tv/downloads")
+j = r.json()
 
-soup = BeautifulSoup(page.text, 'html.parser')
-centos_url = soup.find( attrs={'data-event-label': 'CentOS64'})['href']
+for i in j['computer']['Linux']['releases']:
+    if 'CentOS 64-bit' in i['label']:
+        centos_json = i
+        break
 
-url_path=urlparse(centos_url).path
-# url_path /plex-media-server/0.9.16.6.1993-5089475/plexmediaserver-0.9.16.6.1993-5089475.x86_64.rpm
-
-plex_ver = re.split('\/', url_path)[2]
+# We have full URL:
+# 'https://downloads.plex.tv/plex-media-server/1.3.3.3148-b38628e/plexmediaserver-1.3.3.3148-b38628e.x86_64.rpm
+plex_ver = re.split('\/', centos_json['url'])[4]
 
 print(plex_ver)
